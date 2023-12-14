ac4d3656cec90793725e747ab802d490537ed341
Herman Haggerty <dubh3124@gmail.com>
2023-12-14 13:23:26 -0600

    configuring github action

--- appinfra/github_oidc.tf
+++ appinfra/github_oidc.tf
@@ -1,28 +1,55 @@
-#resource "aws_iam_openid_connect_provider" "github" {
-#  url             = "https://token.actions.githubusercontent.com"
-#  client_id_list  = ["sts.amazonaws.com"]
-#  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
-#}
-#
-#data "aws_iam_policy_document" "github_actions_assume_role" {
-#  statement {
-#    actions = ["sts:AssumeRoleWithWebIdentity"]
-#    principals {
-#      type        = "Federated"
-#      identifiers = [aws_iam_openid_connect_provider.github.arn]
-#    }
-#    condition {
-#      test     = "StringEquals"
-#      variable = "token.actions.githubusercontent.com:aud"
-#      values   = ["sts.amazonaws.com"]
-#    }
-#    condition {
-#      test     = "StringLike"
-#      variable = "token.actions.githubusercontent.com:sub"
-#      values = [
-#        "repo:org1/*:*",
-#        "repo:org2/*:*"
-#      ]
-#    }
-#  }
-#}
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        "JokerProject:dubh3124/*:*"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = "github-actions"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+}
+
+data "aws_iam_policy_document" "github_actions" {
+  statement {
+    actions = [
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:CompleteLayerUpload",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:InitiateLayerUpload",
+      "ecr:PutImage",
+      "ecr:UploadLayerPart",
+    ]
+    resources = ["*"]
+  }
+}
+
+# Give github_actions ECS fully policy role
+resource "aws_iam_role_policy_attachment" "github_actions" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+}
+
+
